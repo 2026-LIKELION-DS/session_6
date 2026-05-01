e9d3d246f2376fe07325f522404579925c75661a
unknown <jooeeeh17@duksung.ac.kr>
2026-05-02 00:26:03 +0900

    [0502] 수정

--- everytime/static/js/main.js
+++ everytime/static/js/main.js
@@ -1,13 +1,21 @@
-window.onload = function() {
-    const writeBtn = document.getElementById('writebtn'); // 연필 버튼
-    const postSection = document.getElementById('post-section'); // 숨겨둔 post 영역
-    const originalBox = document.querySelector('.secret-box3'); // 원래 있던 연필 박스
+document.addEventListener("DOMContentLoaded", function () {
+  const writeBtn = document.getElementById("writebtn");
+  const boardArea = document.getElementById("boardArea");
+  const postArea = document.getElementById("postArea");
 
-    // 버튼이 정상적으로 존재하는지 확인 후 이벤트 등록
-    if (writeBtn) {
-        writeBtn.addEventListener('click', function() {
-            originalBox.style.display = 'none';    
-            postSection.style.display = 'block';   
-        });
-    }
-};
+  writeBtn.addEventListener("click", function () {
+    boardArea.style.display = "none";
+    postArea.style.setProperty("display", "block", "important");
+  });
+});
+
+const questionNotice = document.getElementById("questionNotice");
+const questioncheck = document.getElementById("questioncheck");
+
+questioncheck.addEventListener("change", function () {
+  if (questioncheck.checked) {
+    questionNotice.style.display = "block";
+  } else {
+    questionNotice.style.display = "none";
+  }
+});
