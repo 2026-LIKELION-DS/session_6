7508425fe6e45160e8b031e84a74773bf045d17a
stopwonee <stopw0n@naver.com>
2026-05-08 23:47:55 +0900

    [0508] 에타 연동하기 - 회원가입 to 로그인 to 메인, 로그인 to 메인 안되는거 수정

--- everytime/static/js/signup.js
+++ everytime/static/js/signup.js
@@ -1,30 +1,59 @@
+// const signupForm = document.querySelector('#signupForm');
+// const signupBtn = document.querySelector('#signupBtn');
+
+// const inputs = signupForm.querySelectorAll('.inputBox');
+
+// function handleInput() {
+//     let isAllFilled = true;
+
+//     for (let i = 0; i < inputs.length; i++) {
+//         if (inputs[i].value.trim() === "") {
+//             isAllFilled = false;
+//             break; 
+//         }
+//     }
+
+//     const pwValue = document.querySelector('#userPw').value;
+//     const pwCheckValue = document.querySelector('#userPwCheck').value;
+//     const isPwValid = pwValue.length >= 8 && pwValue === pwCheckValue;
+
+
+//     if (isAllFilled && isPwValid) {
+//         signupBtn.style.backgroundColor = '#f91f15';
+//         signupBtn.disabled = false;
+//     } else {
+//         signupBtn.style.backgroundColor = '#737373';
+//         signupBtn.disabled = true;
+//     }
+// }
+
+// signupForm.addEventListener('input', handleInput);
+
 const signupForm = document.querySelector('#signupForm');
 const signupBtn = document.querySelector('#signupBtn');
-
 const inputs = signupForm.querySelectorAll('.inputBox');
 
 function handleInput() {
-    let isAllFilled = true;
-
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value.trim() === "") {
-            isAllFilled = false;
-            break; 
-        }
-    }
+    const isAllFilled = Array.from(inputs).every(input => input.value.trim() !== "");
 
     const pwValue = document.querySelector('#userPw').value;
     const pwCheckValue = document.querySelector('#userPwCheck').value;
     const isPwValid = pwValue.length >= 8 && pwValue === pwCheckValue;
 
-
     if (isAllFilled && isPwValid) {
         signupBtn.style.backgroundColor = '#f91f15';
         signupBtn.disabled = false;
+        console.log("버튼 활성화됨!");
+        signupBtn.style.cursor = 'pointer';
+        
+        signupBtn.type = "submit"; 
     } else {
         signupBtn.style.backgroundColor = '#737373';
         signupBtn.disabled = true;
+        signupBtn.style.cursor = 'not-allowed';
     }
 }
 
 signupForm.addEventListener('input', handleInput);
+
+handleInput();
